9371060f1ceafadaa9dd7c2528438b4834cfece1
WY-S <67685322+WY-S@users.noreply.github.com>
2023-03-28 18:45:57 +0800

    Update co.php

--- co.php
+++ co.php
@@ -6,7 +6,7 @@ error_reporting (E_ALL|E_STRICT);
 $db = mysqli_init();
 mysqli_options ($db, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
 
-$db->ssl_set(NULL,NULL, "wwwroot/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+$db->ssl_set(NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 $link = mysqli_real_connect ($db, 'wenyimysql.mysql.database.azure.com', 'azureuser', 'SUNwenyi1234', 'school', 3306, NULL, MYSQLI_CLIENT_SSL);
 if (!$link)
 {
