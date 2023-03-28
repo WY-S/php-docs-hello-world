b57ec0461b96528d2c69af0c87d459bca398396f
WY-S <67685322+WY-S@users.noreply.github.com>
2023-03-28 17:02:21 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -3,11 +3,11 @@ $con = mysqli_init();
 if (!$con) {
   die("mysqli_init failed");
 }
-echo "hello";
-mysqli_ssl_set($conn,NULL,NULL, "/home/site/wwwroot/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+echo "hello1";
+mysqli_ssl_set($conn,NULL,NULL, "https://dl.cacerts.digicert.com/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($con,"wenyimysql.mysql.database.azure.com","azureuser","SUNwenyi1234","school",3306,MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-echo "hello";
+echo "hello2";
 ?> 
