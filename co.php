aaa7317be4c9727b9f7b128ecee83bc094af51ab
WY-S <67685322+WY-S@users.noreply.github.com>
2023-03-28 18:40:30 +0800

    Add files via upload

---
+++ co.php
@@ -0,0 +1,19 @@
+<?php
+ini_set ('error_reporting', E_ALL);
+ini_set ('display_errors', '1');
+error_reporting (E_ALL|E_STRICT);
+
+$db = mysqli_init();
+mysqli_options ($db, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+
+$db->ssl_set(NULL,NULL, "wwwroot/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+$link = mysqli_real_connect ($db, 'wenyimysql.mysql.database.azure.com', 'azureuser', 'SUNwenyi1234', 'school', 3306, NULL, MYSQLI_CLIENT_SSL);
+if (!$link)
+{
+    die ('Connect error (' . mysqli_connect_errno() . '): ' . mysqli_connect_error() . "\n");
+} else {
+    $res = $db->query('SHOW TABLES;');
+    print_r ($res);
+    $db->close();
+}
+?>
